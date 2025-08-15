883a7f50fe5d1d93874bc17e155f8d66522ad7bd
Buddy <buddy@Buddys-MacBook-Air.local>
2025-08-15 20:47:55 +0700

    new profile

--- src/services/services.tsx
+++ src/services/services.tsx
@@ -4,7 +4,7 @@ const GetGithub = async (username: string) => {
     try {
         const response = await axios.get(`https://api.github.com/users/${username}/repos`, {
             headers: {
-                Authorization: "token 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",
+                Authorization: `token ${process.env.TOKEN_GITHUB}`,
             },
         });
         return response.data;
